6852da3f591a13ae0f9f72afb100582874f4a9a3
Hau Cao <caohau134@gmail.com>
2026-05-15 22:57:33 +0700

    xin chao

--- PERSONAL-FINANCE-MANAGEMENT-APP/Program.cs
+++ PERSONAL-FINANCE-MANAGEMENT-APP/Program.cs
@@ -10,6 +10,8 @@ namespace PERSONAL_FINANCE_MANAGEMENT_APP
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.WriteLine(123);
         }
     }
 }
