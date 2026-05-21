8c20fffb7d1dd4aab8c0fb0a25f91118bdfa41fb
DuyKhanh55 <kduy1426@gmail.com>
2026-05-21 22:33:50 +0700

    TransactionDAL.cs

--- TransactionDAL.cs
+++ TransactionDAL.cs
@@ -1,12 +1,38 @@
-using System.Collections.Generic;
+using System.Linq;
+using PersonalFinanceApp.DTO;
 
 namespace PersonalFinanceApp.DAL
 {
-    public interface IDataAccess<T>
+    public class TransactionDAL : BaseDAL<TransactionDTO>
     {
-        bool Insert(T item);
-        bool Update(T item);
-        bool Delete(int id);
-        List<T> GetAll();
+        public TransactionDAL() : base("transactions.json") { }
+
+        public override bool Insert(TransactionDTO item)
+        {
+            item.Id = Storage.Any() ? Storage.Max(t => t.Id) + 1 : 1;
+            Storage.Add(item);
+            SaveToFile();
+            return true;
+        }
+
+        public override bool Update(TransactionDTO item)
+        {
+            int index = Storage.FindIndex(t => t.Id == item.Id);
+            if (index == -1) return false;
+            
+            Storage[index] = item;
+            SaveToFile();
+            return true;
+        }
+
+        public override bool Delete(int id)
+        {
+            var item = Storage.Find(t => t.Id == id);
+            if (item == null) return false;
+            
+            Storage.Remove(item);
+            SaveToFile();
+            return true;
+        }
     }
 }
